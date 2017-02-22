cfd9a7d518b42670c56726d6ec7f518d4dcb45af
nemo <603035634@qq.com>
2017-02-22 19:29:44 +0800

    dev 5

--- Indexer4py/Indexer4py.py
+++ Indexer4py/Indexer4py.py
@@ -6,6 +6,7 @@ import httplib
 import time
 import Transmission
 import os
+import sqlite3
     
 if __name__ == '__main__':
     print 'program start.'    
@@ -13,4 +14,13 @@ else:
     print 'program is not a module.'
     exit()
 
-Transmission.mainLoop()
+#Transmission.mainLoop()
+
+cx = sqlite3.connect('data.db')
+cu = cx.cursor()
+cu.execute("insert into test values(4, '%s')" % '你好')
+cx.commit()
+cu.execute('select * from test')
+print cu.fetchall()
+cu.close()
+cx.close()
