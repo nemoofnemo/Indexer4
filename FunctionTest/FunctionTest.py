4daf4c86a85b73e639aa944c0d62f34ef976c487
nemo <603035634@qq.com>
2017-03-08 17:57:10 +0800

    matplotlib

--- FunctionTest/FunctionTest.py
+++ FunctionTest/FunctionTest.py
@@ -1,15 +1,13 @@
 import os
-import time
+#import time
 import multiprocessing
 import codecs 
+import numpy as np
+#import matplotlib as mpl
+import matplotlib.pyplot as plt
+#import seaborn as sns
 
-
-#if __name__ == '__main__':
-#    print '[program start]'    
-#else:
-#    print '[program is not a module]'
-#    exit()
-
-fp = codecs.open('warning.log', 'a+', 'utf-8')
-fp.write('asdfffff')
-fp.close()
+x = [1,2,3]
+y = [40,5,65]
+plt.plot(x,y)
+plt.show()
