123c406b9069c3c845a0e4262940d1904d8fd3d7
nemo <603035634@qq.com>
2017-03-19 16:08:18 +0800

    1

--- FunctionTest/FunctionTest.py
+++ FunctionTest/FunctionTest.py
@@ -7,16 +7,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 #import seaborn as sns
 
-dbconn = sqlite3.connect('data20170228.db')
-cu = dbconn.cursor()
-cu.execute('select * from main_table')
-data = cu.fetchall()
-dbconn.commit()
-dbconn.close()
+#dbconn = sqlite3.connect('data20170228.db')
+#cu = dbconn.cursor()
+#cu.execute('select * from main_table')
+#data = cu.fetchall()
+#dbconn.commit()
+#dbconn.close()
 
-a = [item[2] for item in data]
-plt.plot(a)
-b = [item[3] for item in data]
-plt.plot(b)
+#a = [item[2] for item in data]
+#plt.plot(a)
+#b = [item[3] for item in data]
+#plt.plot(b)
+#plt.show()
+
+b = ['asd','asdf']
+c = [1,2]
+plt.plot(b,c)
 plt.show()
-
